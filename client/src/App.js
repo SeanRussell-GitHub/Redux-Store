301c90470cb657076efd2bd23af665a4d2fa2af0
Sean Russell <emailseanrussell@gmail.com>
2021-10-18 00:24:23 -0700

    refactoring work

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,9 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import store from "./utils/store"
 // import { StoreProvider } from './utils/GlobalState';
 import { Provider } from "react-redux";
+import store from "./utils/store"
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
